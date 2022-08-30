455338d7b768655a79d34bf51cd8bf54ba8f94d9
archer <econarcher2010@gmail.com>
2022-08-30 23:09:20 +0800

    first upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// swiper carousel 
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
