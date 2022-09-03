cc9a81cdc3c3ad958e649aa2ceafe32170d3e7a2
archer <econarcher2010@gmail.com>
2022-09-03 22:17:49 +0800

    make amenities status connect with api

--- roomPage/room.js
+++ roomPage/room.js
@@ -40,7 +40,7 @@ async function fetchData(){
   
   let ary = [];
   try{
-  await axios.get(singleRoom, config)
+  await axios.get(deluxeTwin, config)
     .then((res) => {
       ary = res.data.room;
     });
@@ -90,8 +90,8 @@ async function fetchData(){
             `
             平日（ㄧ～四）價格 : ${ary[0].normalDayPrice} / 
             假日（五～日）價格 : ${ary[0].holidayPrice} (Fri-Sun)<br>
-            入住時間 : ${ary[0].checkInAndOut.checkInEarly} (最早)/
-            ${ary[0].checkInAndOut.checkInLate} (最晚)/<br>
+            入住時間 : ${ary[0].checkInAndOut.checkInEarly} (最早) /
+            ${ary[0].checkInAndOut.checkInLate} (最晚)<br>
             退房時間 : ${ary[0].checkInAndOut.checkOut}
             `
       timeAndPrice.innerHTML = c;
@@ -114,16 +114,149 @@ async function fetchData(){
             `
       desList.innerHTML = d;
 
-
-
-
-
-
-
-
-
-
+// User amenity status to decide which to show
+    const breakfastY = document.querySelector(".breakfastY")
+    const breakfastN = document.querySelector(".breakfastN")    
+    const breakfastBL = ary[0].amenities.Breakfast
+    console.log(breakfastBL);
+    if ( breakfastBL === true ){
+      breakfastN.setAttribute( "style", "display : none" )
     }
+    else{
+      breakfastY.setAttribute( "style", "display : none" )
+    };
+
+    const barY = document.querySelector(".barY")
+    const barN = document.querySelector(".barN")
+    const barBL = ary[0].amenities["Mini-Bar"]
+    console.log(barBL);
+    if ( barBL === true ){
+      barN.setAttribute( "style", "display : none")
+    }
+    else{
+      barY.setAttribute( "style", "display : none" )
+    }
+  
+  const serviceY = document.querySelector(".serviceY")
+  const serviceN = document.querySelector(".serviceN")    
+  const serviceBL = ary[0].amenities["Room-Service"]
+    console.log(serviceBL);
+    if ( serviceBL === true ){
+      serviceN.setAttribute( "style", "display : none" )
+    }
+    else{
+      serviceY.setAttribute( "style", "display : none" )
+    };
+
+  const childY = document.querySelector(".childY")
+  const childN = document.querySelector(".childN")    
+  const childBL = ary[0].amenities["Child-Friendly"]
+    console.log(childBL);
+    if ( childBL === true ){
+      childN.setAttribute( "style", "display : none" )
+    }
+    else{
+      childY.setAttribute( "style", "display : none" )
+    };
+  
+  const wifiY = document.querySelector(".wifiY")
+  const wifiN = document.querySelector(".wifiN")    
+  const wifiBL = ary[0].amenities["Wi-Fi"]
+    console.log(wifiBL);
+    if ( wifiBL === true ){
+      wifiN.setAttribute( "style", "display : none" )
+    }
+    else{
+      wifiY.setAttribute( "style", "display : none" )
+    };
+  
+  const phoneY = document.querySelector(".phoneY")
+  const phoneN = document.querySelector(".phoneN")    
+  const phoneBL = ary[0].amenities["Television"]
+    console.log(wifiBL);
+    if ( wifiBL === true ){
+      phoneN.setAttribute( "style", "display : none" )
+    }
+    else{
+      phoneY.setAttribute( "style", "display : none" )
+    };
+  
+  const beautifulY = document.querySelector(".beautifulY")
+  const beautifulN = document.querySelector(".beautifulN")    
+  const beautifulBL = ary[0].amenities["Great-View"]
+    console.log(wifiBL);
+    if ( wifiBL === true ){
+      beautifulN.setAttribute( "style", "display : none" )
+    }
+    else{
+      beautifulY.setAttribute( "style", "display : none" )
+    };
+
+  const fridgeY = document.querySelector(".fridgeY")
+  const fridgeN = document.querySelector(".fridgeN")    
+  const fridgeBL = ary[0].amenities["Refrigerator"]
+    console.log(wifiBL);
+    if ( fridgeBL === true ){
+      fridgeN.setAttribute( "style", "display : none" )
+    }
+    else{
+      fridgeY.setAttribute( "style", "display : none" )
+    };
+  
+  const sofaY = document.querySelector(".sofaY")
+  const sofaN = document.querySelector(".sofaN")    
+  const sofaBL = ary[0].amenities["Sofa"]
+    console.log(wifiBL);
+    if ( sofaBL === true ){
+      sofaN.setAttribute( "style", "display : none" )
+    }
+    else{
+      sofaY.setAttribute( "style", "display : none" )
+    };
+  
+  const petY = document.querySelector(".petY")
+  const petN = document.querySelector(".petN")    
+  const petBL = ary[0].amenities["Pet-Friendly"]
+      // console.log(wifiBL);
+    if ( petBL === true ){
+      petN.setAttribute( "style", "display : none" )
+    }
+    else{
+      petY.setAttribute( "style", "display : none" )
+    };
+  
+  const smokingY = document.querySelector(".smokingY")
+  const smokingN = document.querySelector(".smokingN")    
+  const smokingBL = ary[0].amenities["Smoke-Free"]
+      // console.log(wifiBL);
+    if ( smokingBL === true ){
+      smokingY.setAttribute( "style", "display : none" )
+    }
+    else{
+      smokingN.setAttribute( "style", "display : none" )
+    };
+
+  const acY = document.querySelector(".acY")
+  const acN = document.querySelector(".acN")    
+  const acBL = ary[0].amenities["Air-Conditioner"]
+      // console.log(wifiBL);
+    if ( acBL === true ){
+      acN.setAttribute( "style", "display : none" )
+    }
+    else{
+      acY.setAttribute( "style", "display : none" )
+    };
+
+
+
+
+
+
+
+
+
+  }
+
     roomDetail()
 
     //...
